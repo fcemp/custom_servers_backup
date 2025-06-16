8a5e2873ffd0ec3db8e3912464ff83cc1b13282a
Athul <fcemp@containers.flexicloudhosting.com>
2025-06-16 16:34:25 +0530

    removed yml/ and test/

--- test/test_function.sh
+++
@@ -1,9 +0,0 @@
-YAML_FILE="/backup/scripts/custom_servers_backup/test/servers.yml"
-YQ_PATH="/usr/bin/yq"
-
-function get_server_name(){
-	        SERVER_KEY=$1
-		        echo "$(cat $YAML_FILE |$YQ_PATH e ".servers.$SERVER_KEY.name"|tr -d "[:blank:]")"
-}
-
-get_server_name '39'
